74c8c26d8e2379f18d2a1f76cac4b56f24776ec1
madalitsomchaina <mmchain@uwo,ca>
2017-08-11 14:43:12 -0400

    first commit

---
+++ GrowLight.cpp
@@ -0,0 +1,35 @@
+#include "GrowLight.h"
+#include "HardwareSerial.h" //Arduino Hardware Library
+#include "WProgram.h" //Core Wiring API
+
+
+GrowLight::GrowLight( int pinNum, int _interval) {
+    interval = _interval;
+    pinNumber = pinNum;
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+    pinState = false;
+    stopTime = 0;
+}
+
+void GrowLight::on() {
+    digitalWrite(pinNumber, HIGH);
+    pinState = true;
+}
+
+void GrowLight::off() {
+    digitalWrite(pinNumber, LOW);
+    pinState = false;
+}
+
+void GrowLight::changeInterval(int newInterval) {
+    interval = newInterval;
+}
+
+void GrowLight::strobe() {
+    if (stopTime =< millis()) {
+        if(pinState) this.off();
+        else this.on();
+        stopTime = millis() + interval;
+    }
+}
