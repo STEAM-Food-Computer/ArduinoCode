74b701b73d6559e6a9bb47a3559671f354a0d2bf
madalitsomchaina <mmchain@uwo,ca>
2017-09-01 23:35:39 -0400

    Creating WaterSensor Class

---
+++ libraries/WaterSensor/WaterSensor.cpp
@@ -0,0 +1,34 @@
+﻿#include "WaterSensor.h"
+
+WaterSensor::WaterSensor(char digPinNum, char anPinNum) {
+    digitalPinNumber = digPinNum;
+    analogPinNumber = anPinNum;
+}
+
+void WaterSensor::on() {
+    digitalWrite(digitalPinNumber, HIGH)
+    pinState = true;
+}
+
+void WaterSensor::off() {
+    digitalWrite(digitalPinNumber, LOW)
+    pinState = false;
+}
+short WaterSensor::poll() {
+
+    if(!pinState) {
+        this->on();
+        stopTime = millis() + stabilizingInterval;
+        return -1; //if a negative is returned, then the sensor is not ready to be read from
+    }
+
+    else {
+        if(millis() > stopTime) {
+            short readValue = (short)analogRead(analogPinNumber);
+            this->off();
+            return readValue;
+        }
+        else return -1;
+    }
+
+}
---
+++ libraries/WaterSensor/WaterSensor.h
@@ -0,0 +1,28 @@
+﻿#ifndef WaterSensor_h
+#define WaterSensor_h
+#if defined(ARDUINO) && ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#include <pins_arduino.h>
+#endif
+
+class WaterSensor {
+
+    char digitalPinNumber;  //keeps track of the Vcc pin connected to the particular sensor
+    char analogPinNumber;   //keeps track of the pin the sensor is read from
+    short lastRawReading;
+    const short stabilizingInterval = 20000;    //time it takes for sensor reading to stabilize
+    bool pinState;  //keep track of if the pin is high or low
+    long stopTime;
+
+    public:
+        WaterSensor(char digPinNum, anPinNum);
+        short poll();   //initiate the process of reading the sensor
+        void on();  //Vcc is on
+        void off(); //Vcc is off
+};
+#endif
+/*
+    Turning on and off the Vcc to read the sensor reduces corrosion on the sensor
+*/
